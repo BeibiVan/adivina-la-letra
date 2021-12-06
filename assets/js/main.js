48875cf0dae82b19ffb90f3b63a523e82386b7f2
Ivan Dziuba <ivaan.id@gmail.com>
2021-12-06 11:22:26 -0300

    finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+//// first section
+$("#primera-letra_boton2").click (function (){
+    $("#primera-letra_boton2").css ({"background-color": "green"})
+    ({"color": "white"})
+})
+
+$("#primera-letra_boton1").click (function (){
+    $("#primera-letra_boton1").css ({"background-color": "red"})
+    ({"color": "white"})
+})
+
+$("#primera-letra_boton3").click (function (){
+    $("#primera-letra_boton3").css ({"background-color": "red"})
+    ({"color": "white"})
+})
+
+///second section
+
+$("#segunda-letra_boton1").click (function (){
+    $("#segunda-letra_boton1").css ({"background-color": "green"})
+    ({"color": "white"})
+})
+
+$("#segunda-letra_boton2").click (function (){
+    $("#segunda-letra_boton2").css ({"background-color": "red"})
+    ({"color": "white"})
+})
+
+$("#segunda-letra_boton3").click (function (){
+    $("#segunda-letra_boton3").css ({"background-color": "red"})
+    ({"color": "white"})
+})
+
